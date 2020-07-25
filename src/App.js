2d541a2c82be098836cce5b4497abcccb017260b
Heimax01 <heitor_campeao@hotmail.com>
2020-07-25 16:32:24 -0300

    update request

--- src/App.js
+++ src/App.js
@@ -9,14 +9,9 @@ function App() {
 
 
     const listData =  async () => {
-<<<<<<< HEAD
        const response = await api.post('/listEventTypes',{
         filter: {}
         //eventTypeIds:["1"],marketCountries:["FI"]
-=======
-       const response = await api.post('/listEvents',{
-        filter: {eventTypeIds:["1"],marketCountries:["FI"]}
->>>>>>> cb57f8a23a89fddbc0d139f2840f37be09cac6d4
       })
 
       console.log(response.data)
--- src/services/api.js
+++ src/services/api.js
@@ -3,7 +3,6 @@ import axios from 'axios';
 const api = axios.create({
   baseURL: 'https://api.betfair.com/exchange/betting/rest/v1.0',
   headers: {
-<<<<<<< HEAD
     
    'X-Application': 'C1l1lzS5QDKHsXqp',
    'Content-Type': 'application/json',
@@ -13,16 +12,6 @@ const api = axios.create({
  
 }
   
-=======
-    post: {
-      'Accept':'application/json',
-      'Content-Type': 'application/json',
-      'X-Application': 'C1l1lzS5QDKHsXqp',
-      'X-Authentication': 'tyhvUoWpbjX/MRg1GSEx99ah6Sdggfb6HaWNHXiSe80=',
-    }
-    
-   }
->>>>>>> cb57f8a23a89fddbc0d139f2840f37be09cac6d4
 });
 
 export default api;
