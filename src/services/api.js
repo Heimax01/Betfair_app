d23d7aa115d025d205473b13d2467538a03624a1
Heimax01 <heitor_campeao@hotmail.com>
2020-07-24 22:30:32 -0300

    fix: Axios BaseURL

--- src/services/api.js
+++ src/services/api.js
@@ -2,6 +2,15 @@ import axios from 'axios';
 
 const api = axios.create({
   baseURL: 'https://api.betfair.com/exchange/betting/rest/v1.0',
+  headers: {
+    post: {
+      'Accept':'application/json',
+      'Content-Type': 'application/json',
+      'X-Application': 'C1l1lzS5QDKHsXqp',
+      'X-Authentication': 'tyhvUoWpbjX/MRg1GSEx99ah6Sdggfb6HaWNHXiSe80=',
+    }
+    
+   }
 });
 
 export default api;
