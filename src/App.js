4584d6ce00b2aaf529f5898cb5d483f797fc0431
Heimax01 <heitor_campeao@hotmail.com>
2020-07-25 17:02:48 -0300

    request config

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import React, {useEffect} from 'react';
-import api from './services/api';
+import axios from 'axios';
+// import api from './services/api';
 
 function App() {
 
@@ -9,13 +10,37 @@ function App() {
 
 
     const listData =  async () => {
-       const response = await api.post('/listEventTypes',{
-        filter: {}
-        //eventTypeIds:["1"],marketCountries:["FI"]
-      })
 
-      console.log(response.data)
-    }
+      try {
+
+        const data = {
+          filter: {}
+          //eventTypeIds:["1"],marketCountries:["FI"]
+        };
+
+        const config = {
+          headers: {
+            'Content-Type': 'application/json',
+            'X-Application': 'C1l1lzS5QDKHsXqp',
+            'X-Authentication': '92i7ocKfGzh89M4KcimsWQ/obZIpAvbA+ybWSNcIFdE=',
+          }
+        };             
+
+
+      const response = await axios.post('https://api.betfair.com/exchange/betting/rest/v1.0/listEventTypes',data, config)
+
+      console.log(`Status: ${response.status}`);
+      console.log(`Content Type: ${response.headers['Content-Type']}`);
+      console.log(`X-Application: ${response.headers['X-Application']}`);
+      console.log(`X-Authentication: ${response.headers['X-Authentication']}`);          
+      
+
+      
+
+        
+    }  catch (err) {
+    console.error(err);
+  }}
    
     listData()
     
--- src/services/api.js
+++ src/services/api.js
@@ -2,16 +2,6 @@ import axios from 'axios';
 
 const api = axios.create({
   baseURL: 'https://api.betfair.com/exchange/betting/rest/v1.0',
-  headers: {
-    
-   'X-Application': 'C1l1lzS5QDKHsXqp',
-   'Content-Type': 'application/json',
-   'X-Authentication': '92i7ocKfGzh89M4KcimsWQ/obZIpAvbA+ybWSNcIFdE=',
-   
- 
- 
-}
-  
 });
 
 export default api;
